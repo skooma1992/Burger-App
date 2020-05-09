24d6150fd55b10a1e0bbb5daaed4885e750d4bf7
Jake <upsonjake@gmail.com>
2020-05-09 16:39:53 -0400

    One more time

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE `burgers`
     id INTEGER AUTO_INCREMENT,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NUll,
     PRIMARY KEY (id)
 );
      
